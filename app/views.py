f8bc7832cd51dc917c686a419cac8509ed0e9f4e
jeonyoonji <yj.jeon.gn@gmail.com>
2022-04-29 00:43:52 +0900

    map js

--- app/views.py
+++ app/views.py
@@ -24,29 +24,29 @@ def event(request):
 def index(request):
     return render(request, 'app/index.html') 
 
-def login(request):
-    return render(request, 'app/login.html')
+# def login(request):
+#     return render(request, 'app/login.html')
 
-def signup_selecttype(request):
-    return render(request, 'app/signup_selecttype.html')
+# def signup_selecttype(request):
+#     return render(request, 'app/signup_selecttype.html')
 
-def signup_com(request):
-    return render(request, 'app/signup_com.html')
+# def signup_com(request):
+#     return render(request, 'app/signup_com.html')
 
-def signup_official(request):
-    return render(request, 'app/signup_official.html')
+# def signup_official(request):
+#     return render(request, 'app/signup_official.html')
 
-def service_writework(request):
-    return render(request, 'app/service_writework.html')
+# def service_writework(request):
+#     return render(request, 'app/service_writework.html')
 
-def board_list(request):
-    return render(request, 'app/board_list.html')
+# def board_list(request):
+#     return render(request, 'app/board_list.html')
 
-def map(request):
-    return render(request, 'app/map.html')
+# def map(request):
+#     return render(request, 'app/map.html')
 
-def pwchange(request):
-    return render(request, 'app/pwchange.html')
+# def pwchange(request):
+#     return render(request, 'app/pwchange.html')
 
-def mypage(request):
-    return render(request, 'app/mypage.html')
+# def mypage(request):
+#     return render(request, 'app/mypage.html')
